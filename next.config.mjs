1e1f2b9adffb5dca0351a569ba625cf2a38ab924
Hibbanur-Rahman <hibbanrahmanhyt@gmail.com>
2024-06-01 16:28:53 +0530

    adding the setup of deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output:"export",
+    basePath:'/to-do-next',
+    assetPrefix: '/to-do-next/',
+    images:{
+        unoptimized:true,
+    }
+};
 
 export default nextConfig;
